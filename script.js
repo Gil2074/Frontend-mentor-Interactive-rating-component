6a05557771187050dd6d3a00bbe676a2fbfe56d1
Gil2074 <charfeddinemhamd@outlook.com>
2023-07-05 16:20:53 +0100

    refactored some css and js

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ function sendRating() {
   event.preventDefault(); // Prevent form submission
   if (selectedRating !== null) {
     const myString = `\n\n <span> Thank you!</span> \n\nWe appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!`;
-    document.getElementById('notification').innerHTML = myString;
-    document.getElementById('notification').setAttribute( 'style', 'text-align:center' );
+    let notification = document.getElementById('notification');
+    let ratingelts = document.getElementById('idrating') ;
+    notification.innerHTML = myString;
+    notification.setAttribute( 'class', 'text-center' );
     document.querySelector('h2').innerHTML =`<img src="./images/illustration-thank-you.svg" alt="">`;
-    document.getElementById('idrating').innerHTML = `You selected ${selectedRating} out of 5;`
-    document.getElementById('idrating').setAttribute( 'class', 'onlyJs' );
+    document.querySelector('h2').setAttribute( 'class', 'text-center' );
+    ratingelts.innerHTML = `You selected ${selectedRating} out of 5;`
+    ratingelts.setAttribute( 'class', 'onlyJs' );
     document.querySelector(`input[type='submit']`).setAttribute( 'class', 'hidden' );
     document.querySelector(`div.container`).setAttribute( 'class', 'hidden' );
     document.querySelector(`div.image`).setAttribute( 'class', 'hidden' );
